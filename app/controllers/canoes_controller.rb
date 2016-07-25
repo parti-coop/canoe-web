5df845a2b6350af4dc0d1636b1977359b9bff375
rest515 <rest515@gmail.com>
2016-07-25 19:56:56 +0900

    canoes list

--- app/controllers/canoes_controller.rb
+++ app/controllers/canoes_controller.rb
@@ -2,6 +2,10 @@ class CanoesController < ApplicationController
   before_filter :authenticate_user!
   load_and_authorize_resource
 
+  def index
+    @canoes = Canoe.order(:title)
+  end
+
   def create
     # TODO current_user를 가입시켜야 합니다.
     @canoe.user = current_user
