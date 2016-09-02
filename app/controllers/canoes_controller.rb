beae3d547414d1ef2b2050540eba606397631cc9
rest515 <rest515@gmail.com>
2016-09-02 10:02:48 +0900

    카누를 수정합니다

--- app/controllers/canoes_controller.rb
+++ app/controllers/canoes_controller.rb
@@ -6,12 +6,29 @@ class CanoesController < ApplicationController
     @canoes = Canoe.order(:title)
   end
 
+  def new
+    @canoe = Canoe.new
+  end
+
   def create
     @canoe.user = current_user
     @canoe.join(current_user)
     @canoe.save
 
     redirect_to @canoe
+  end
+
+  def edit
+    @canoe = Canoe.find(params[:id])
+  end
+
+  def update
+    @canoe = Canoe.find(params[:id])
+    if @canoe.update(canoe_params)
+      redirect_to @canoe
+    else
+      render 'update'
+    end
   end
 
   private
