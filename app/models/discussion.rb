b70b01bf28a0484ddc3b28c7ff70cebe73629ba9
rest515 <rest515@gmail.com>
2016-09-02 15:42:51 +0900

    카누 홈의 논의와 항해일지 정렬을 시간역순으로 합니다

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -3,4 +3,6 @@ class Discussion < ApplicationRecord
   belongs_to :canoe
   has_many :opinions, dependent: :destroy
   has_many :proposal_requests, dependent: :destroy
+
+  scope :recent, -> { order(created_at: :desc) }
 end
--- app/models/sailing_diary.rb
+++ app/models/sailing_diary.rb
@@ -1,4 +1,6 @@
 class SailingDiary < ApplicationRecord
   belongs_to :user
   belongs_to :canoe
+
+  scope :recent, -> { order(created_at: :desc) }
 end
