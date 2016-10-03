7925e30d2d79318f08ef7c724044e1beb8b59ca4
qus <qus1225@gmail.com>
2016-10-03 13:00:22 +0900

    구동환경 설정에서의 도메인네임을 바꿉니다 #347

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,4 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 end
 
-Rails.application.routes.default_url_options[:host] = 'canoe.parti.xyz'
+Rails.application.routes.default_url_options[:host] = 'canoe.ws'
