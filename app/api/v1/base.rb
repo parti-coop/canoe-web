8eb800ee0ccd0c724ba6c4fe797027c2ea775d2b
dali <dalikim@parti.xyz>
2016-10-10 22:06:31 +0900

    카누 조회하는 API를 만듭니다.

--- app/api/v1/base.rb
+++ app/api/v1/base.rb
@@ -1,8 +1,51 @@
 module V1
   module Entities
+    class EmotionEntity < Grape::Entity
+      expose :id, :sign
+      expose :text do |emotion|
+        emotion.sign_text
+      end
+    end
+
+    class UserEntity < Grape::Entity
+      expose :email, :image_url, :nickname
+    end
+
+    class CategoryEntity < Grape::Entity
+      expose :name, :id
+    end
+
+    class CommentEntity < Grape::Entity
+      expose :id, :body, :created_at
+      expose :user, using: V1::Entities::UserEntity
+    end
+
+    class DiscussionEntity < Grape::Entity
+      expose :id, :title, :body, :opinions_count, :archived_at, :created_at
+      expose :category, using: V1::Entities::CategoryEntity
+      expose :user, using: V1::Entities::UserEntity
+    end
+
+    class SailingDiaryEntity < Grape::Entity
+      expose :id, :body, :sailed_on, :created_at
+      expose :user, using: V1::Entities::UserEntity
+      expose :comments, using: V1::Entities::CommentEntity
+      expose :emotions, using: V1::Entities::EmotionEntity
+    end
+
     class CanoeEntity < Grape::Entity
-      expose :id
-      expose :title
+      expose :id, :title, :body, :logo_url, :boarding_requests_count, :created_at
+      expose :user, using: V1::Entities::UserEntity
+    end
+
+    class CanoeDetailEntity < CanoeEntity
+      expose :categories, using: V1::Entities::CategoryEntity
+      expose :recent_discussions, using: V1::Entities::DiscussionEntity do |canoe|
+        canoe.discussions.recent.limit(10)
+      end
+      expose :recent_sailing_diaries, using: V1::Entities::SailingDiaryEntity do |canoe|
+        canoe.sailing_diaries.recent.limit(10)
+      end
     end
   end
 
@@ -21,6 +64,17 @@ module V1
     get :canoes do
       present :canoes, Canoe.all, with: V1::Entities::CanoeEntity
     end
+
+    resource :canoes do
+      desc '카누 상세 정보를 조회합니다'
+      route_param :id do
+        get do
+          canoe = Canoe.find(params[:id])
+          present :canoe, canoe, with: V1::Entities::CanoeDetailEntity
+        end
+      end
+    end
+
 
     add_swagger_documentation \
       hide_documentation_path: true,
